278fb82dfd0b68dff677e5dba124a869febe13cc
big-kahuna-burger <arandjel.sarenac@gmail.com>
2023-11-28 09:52:27 +0100

    ci: flip logic

--- should-run-test-suites.js
+++ should-run-test-suites.js
@@ -1,6 +1,6 @@
 const nodeMajorVersion = process.versions.node.split('.').map(x => parseInt(x, 10))[0]
-const code = nodeMajorVersion < 20 ? 0 : 1
-if (!code) {
+const shouldRunSuites = nodeMajorVersion >= 20
+if (!shouldRunSuites) {
   console.info(`Skipped templates test suites on node ${nodeMajorVersion}`)
+  process.exit(1)
 }
-process.exit(code)
